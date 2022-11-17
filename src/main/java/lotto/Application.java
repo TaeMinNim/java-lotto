4c1f2fc7596fcade27e8a44f09a3c23693ba8370
TaeMin <xoals3094@naver.com>
2022-11-18 08:20:57 +0900

    refactor(Application) : Controller 리펙토링에 따라 main 메소드 재구성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,19 +1,19 @@
 package lotto;
 
+import lotto.Controller.CreateLottoController;
+import lotto.Controller.InputController;
 import lotto.Controller.ResultController;
 
 public class Application {
     public static void main(String[] args) {
-        ResultController mainController = new ResultController();
+        InputController inputController = new InputController();
+        CreateLottoController createLottoController = new CreateLottoController();
+        ResultController resultController = new ResultController();
         try {
-            mainController.inputCash();
-            mainController.createMyLottos();
-            mainController.printMyLottos();
-
-            mainController.inputWinningLotto();
-            mainController.calculateWinning();
-
-            mainController.printResult();
+            inputController.inputCash();
+            createLottoController.createMyLotto();
+            inputController.inputWinningLotto();
+            resultController.checkResult();
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] " + e.getMessage());
         }
