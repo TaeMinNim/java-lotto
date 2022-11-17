d7780e0069ef63aed984b3386c097a64967941ab
TaeMin <xoals3094@naver.com>
2022-11-18 08:16:50 +0900

    feat(ResultController) : 결과 조회 메소드 구현

--- src/main/java/lotto/Controller/ResultController.java
+++ src/main/java/lotto/Controller/ResultController.java
@@ -8,8 +8,13 @@ import lotto.View.OutputView;
 import java.util.HashMap;
 
 public class ResultController {
-    private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
     private Singleton storage = Singleton.getInstance();
 
+    public void checkResult(){
+        HashMap<Rank, Integer> point = storage.myLotto.checkWinning(storage.winningLotto);
+        int totalPrize = storage.myLotto.calculateTotalPrize(point);
+        double profit = storage.cash.calculateProfit(totalPrize);
+        outputView.printResult(point, profit);
+    }
 }
