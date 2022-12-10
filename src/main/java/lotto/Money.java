8f3e99097226d2482c4cd5f10167377cd6069d0f
TaeMinNim <xoals3094@naver.com>
2022-12-11 01:51:52 +0900

    Feat(Money) : 래퍼 클래스 생성 및 예외처리

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,26 @@
+package lotto;
+
+public class Money {
+    private final static int UNIT = 1000;
+    private int money;
+
+    public Money(String string){
+        validData(string);
+        this.money = Integer.parseInt(string);
+    }
+
+    private void validData(String string){
+        int money;
+        try{
+            money = Integer.parseInt(string);
+        } catch(NumberFormatException e){
+            throw new IllegalArgumentException("올바른 값을 입력해주세요.");
+        }
+        if(money < 1000){
+            throw new IllegalArgumentException("1000원 이상의 값을 입력해주세요.");
+        }
+        if(!(money % UNIT == 0)){
+            throw new IllegalArgumentException("1000원 단위의 값을 입력해주세요.");
+        }
+    }
+}
