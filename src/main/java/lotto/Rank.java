46d84af011bd1dd2234cddbf063d686b0f274ebe
TaeMinNim <xoals3094@naver.com>
2022-12-11 02:35:19 +0900

    Feat(Rank) : 일치 개수와 보너스 번호 포함 여부로 랭크를 확인하는 기능 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -5,12 +5,29 @@ public enum Rank {
     SECOND(30_000_000, 5),
     THIRD(1_500_000, 5),
     FORTH(50_000, 4),
-    FIFTH(5_000, 3);
-
+    FIFTH(5_000, 3),
+    LAST(0, 0);
     private final int prize;
     private final int match;
     Rank(int prize, int match){
         this.prize = prize;
         this.match = match;
+    }
+
+    public static Rank checkRank(int match, boolean bonus){
+        if(match == SECOND.match){
+            if(bonus){
+                return SECOND;
+            }
+            return THIRD;
+        }
+
+        for(Rank rank: Rank.values()){
+            if(rank.match == match){
+                return rank;
+            }
+        }
+
+        return LAST;
     }
 }
