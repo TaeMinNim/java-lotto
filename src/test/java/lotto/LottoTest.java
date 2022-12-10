62baeea7dbc726a864b82f6860f209f1d6800e4a
TaeMinNim <xoals3094@naver.com>
2022-12-11 03:03:53 +0900

    Test(Lotto) : countMatch 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,10 +2,14 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +27,25 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    private Lotto winningLotto = new Lotto(List.of(1,2,3,4,5,6));
+    private int bonus = 7;
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void countMatchTest(List<Integer> numbers, int match){
+        Lotto lotto = new Lotto(numbers);
+        int actual = winningLotto.countMatch(lotto);
+        assertThat(actual).isEqualTo(match);
+    }
+
+    static Stream<Arguments> generateData(){
+        return Stream.of(
+                Arguments.of(List.of(6,5,4,3,2,1), 6),
+                Arguments.of(List.of(7,6,5,4,3,2), 5),
+                Arguments.of(List.of(8,7,6,5,4,3), 4),
+                Arguments.of(List.of(9,8,7,6,5,4), 3),
+                Arguments.of(List.of(10,9,8,7,6,5), 2),
+                Arguments.of(List.of(11,10,9,8,7,6), 1),
+                Arguments.of(List.of(12,11,10,9,8,7), 0)
+        );
+    }
 }
