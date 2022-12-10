1fa9df107b40a925a85dbfd7cbd5964abff979c1
TaeMinNim <xoals3094@naver.com>
2022-12-11 02:08:44 +0900

    Feat(LottoList) : 발행한 로또를 관리하는 LottoList 클래스를 생성합니다

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
-    public List<Lotto> createLotto(int count){
+    public LottoList createLotto(int count){
         List<Lotto> lottoList = new ArrayList<>();
         for(int i = 0 ; i < count; i++){
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             lottoList.add(new Lotto(numbers));
         }
 
-        return lottoList;
+        return new LottoList(lottoList);
     }
 }
---
+++ src/main/java/lotto/LottoList.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoList {
+    private List<Lotto> lottoList;
+
+    public LottoList(List<Lotto> lottoList){
+        this.lottoList = lottoList;
+    }
+}
