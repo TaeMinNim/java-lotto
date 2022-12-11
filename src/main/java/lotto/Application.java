028b66ef0a3837bc78c3befa41e95abb910d5811
TaeMinNim <xoals3094@naver.com>
2022-12-11 14:59:35 +0900

    Feat : 기능 결합

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,12 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller();
+        try {
+            controller.createLotto();
+            controller.checkWinning();
+        } catch (IllegalArgumentException e){
+            System.out.println("[ERROR] " + e.getMessage());
+        }
     }
 }
