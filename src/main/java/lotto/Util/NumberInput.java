f781a69ea6a1b31f5e5a331a1ea0be8542ef3fd7
TaeMinNim <xoals3094@naver.com>
2022-12-11 14:34:45 +0900

    Feat : 파싱과 유효값 검증을 분할합니다

---
+++ src/main/java/lotto/Util/NumberInput.java
@@ -0,0 +1,35 @@
+package lotto.Util;
+
+public class NumberInput {
+    private final static int MIN = 1;
+    private final static int MAX = 45;
+
+    private final int number;
+
+    public NumberInput(String numberString){
+        int number = trans(numberString);
+        validData(number);
+        this.number = number;
+    }
+
+    private int trans(String numberString) throws IllegalArgumentException{
+        int number;
+        try{
+            number = Integer.parseInt(numberString);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("올바른 값을 입력해주세요");
+        }
+
+        return number;
+    }
+
+    private void validData(int number){
+        if(!(number >= MIN && number <= MAX)){
+            throw new IllegalArgumentException("1~45 사이의 숫자이어야 합니다.");
+        }
+    }
+
+    public int getNumber(){
+        return number;
+    }
+}
--- src/main/java/lotto/Util/NumbersInput.java
+++ src/main/java/lotto/Util/NumbersInput.java
@@ -1,23 +1,28 @@
-package lotto;
+package lotto.Util;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class Numbers {
+public class NumbersInput {
     private List<Integer> numbers;
-    public Numbers(String string) {
-        numbers = new ArrayList<>();
+    public NumbersInput(String string) {
+        numbers = trans(string);
+    }
+
+    private List<Integer> trans(String string) throws IllegalArgumentException{
+        List<Integer> numbers = new ArrayList<>();
         try {
             String[] strings = string.split(",");
             for (int i = 0; i < strings.length; i++) {
-                Num num = new Num(Integer.parseInt(strings[i]));
-                numbers.add(num.getNum());
+                NumberInput number = new NumberInput(strings[i]);
+                numbers.add(number.getNumber());
             }
 
         } catch (Exception e) {
             throw new IllegalArgumentException("올바른 값을 입력하세요.");
         }
+
+        return numbers;
     }
     public List<Integer> getNumbers() {
         return numbers;
